a14398a0984fdd5f0ab95193a07689654e0df96c
Denis TDR <denis.tdr@gmail.com>
2018-05-13 10:44:55 +0300

    some css fixes

--- src/app/pages/articles-page/articles-page.component.ts
+++ src/app/pages/articles-page/articles-page.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Article} from '../../models/article';
 import {ApiService} from '../../services/api.service';
-import { MatDialog } from '@angular/material';
-import { AddArticleComponent } from '../../components/add-article/add-article.component';
+import {MatDialog} from '@angular/material';
+import {AddArticleComponent} from '../../components/add-article/add-article.component';
 
 @Component({
   selector: 'app-articles-page',
@@ -10,48 +10,50 @@ import { AddArticleComponent } from '../../components/add-article/add-article.co
   styleUrls: ['./articles-page.component.scss']
 })
 export class ArticlesPageComponent implements OnInit {
-   public articles: Article[];
-   public filteredItems: Article[];
-   public loadingArticles = false;
-    constructor( private api: ApiService,
-      private addNewArticleDialog: MatDialog) {
-    }
+  public articles: Article[];
+  public filteredItems: Article[];
+  public loadingArticles = false;
 
-    ngOnInit() {
-      this.getArticles();
-    }
-
-    getArticles()
-    {
-      this.loadingArticles = true;
-      this.api.getArticles().subscribe(
-        ( data: any ) => {
-          this.articles = <Article[]> data.result;
-          this.assignCopy();//when you fetch collection from server.
-          this.loadingArticles = false;
-        },
-        error => {
-          this.loadingArticles = false;
-          console.log(error);
-        }
-      );
-    }
-
-    public openAddArticleModal() {
-      const dialogRef = this.addNewArticleDialog.open(AddArticleComponent, {width: '400px'});
-      dialogRef.afterClosed().subscribe(res => {
-        if(res == "OK")
-          this.getArticles();
-      });
-    }
-
-    assignCopy(){
-      this.filteredItems = Object.assign([], this.articles);
-    }
-    filterItem(value){
-        if(!value) this.assignCopy(); //when nothing has typed
-        this.filteredItems = Object.assign([], this.articles).filter(
-          item => item.title.toLowerCase().indexOf(value.toLowerCase()) > -1
-        )
-    }
+  constructor(private api: ApiService,
+              private addNewArticleDialog: MatDialog) {
   }
+
+  ngOnInit() {
+    this.getArticles();
+  }
+
+  getArticles() {
+    this.loadingArticles = true;
+    this.api.getArticles().subscribe(
+      (data: any) => {
+        this.articles = <Article[]> data.result;
+        this.assignCopy(); // when you fetch collection from server.
+        this.loadingArticles = false;
+      },
+      error => {
+        this.loadingArticles = false;
+        console.log(error);
+      }
+    );
+  }
+
+  public openAddArticleModal() {
+    const dialogRef = this.addNewArticleDialog.open(AddArticleComponent, {width: '400px'});
+    dialogRef.afterClosed().subscribe(res => {
+      if (res === 'OK') {
+        this.getArticles();
+      }
+    });
+  }
+
+  assignCopy() {
+    this.filteredItems = Object.assign([], this.articles);
+  }
+
+  filterItem(value) {
+    if (!value) { this.assignCopy(); } // when nothing has typed
+    this.filteredItems = Object.assign([], this.articles).filter(
+      item => item.title.toLowerCase().indexOf(value.toLowerCase()) > -1
+    );
+  }
+}
