558c3c1fb239d6d494434a644aed7432d0161723
darlegheorghe <darlegheorghe@yahoo.ro>
2018-05-13 09:11:33 +0300

    improvments

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean;
+  public profile: any;
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.getProfile();
   }
 
+  private getProfile() {
+    this.loading = true;
+    this.api.getProfile().subscribe(
+      ( data: any ) => {
+        this.profile = data.result;
+        console.log(this.profile);
+        this.profile.birthdate = this.profile.birthdate.split("T")[0];
+        this.loading = false;
+      },
+      error => {
+        this.loading = false;
+      }
+    );
+  }
 }
